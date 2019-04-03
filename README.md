42a012be4ec32a04b22ab37b373df13cf77cfe68
Hubert Skrzypczak <hskrzypczak98@gmail.com>
2019-04-03 18:50:52 +0200

    Add script for train with 3 differents approaches, create generator_2 (different size of kernels

--- README.md
+++ README.md
@@ -2,15 +2,11 @@
 This repo contains TensorFlow implementation of DCGAN for Image Completion
 
 @TODO:
-  - more complex discriminator,
-  - better losses
-  - function for create dataset from images as files
-  - function for generate random size and position masks
   - visualize trainging process
   - and what ever it takes to enhance performance of GAN :D
   
   
 Members of project team:
-  -Adam Stafiej
+  - Adam Stafiej
   - Andrzej Świerczyński
   - Hubert Skrzypczak
--- playground.py
+++ playground.py
@@ -7,10 +7,11 @@ import tensorflow as tf
 tf.enable_eager_execution()
 
 gen = generator((128, 128, 3))
-gen.load_weights("./weights/generator.h5")
+# gen.load_weights("./weights/generator_epoch24_metrics0.89296875, 0.921875, 0.10703125, 2025.21171875.h5")
+gen.load_weights("./weights/generator_epoch29_metrics0.994140625, 0.9671875, 0.005859375, 2014.2529296875.h5")
 
-data_gen = dataset_generator(image_dimensions=(128, 128), directory="./images",
-                             min_mask=10, max_mask=10, rotation=10, batch_size=3)
+data_gen = dataset_generator(image_dimensions=(256, 256), directory="./images",
+                             min_mask=10, max_mask=80, rotation=15, batch_size=3)
 data_gen.generate_dataset()
 
 images, masks = data_gen.get_batch()
